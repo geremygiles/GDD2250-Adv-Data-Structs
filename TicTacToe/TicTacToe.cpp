7d83159a114296d666024f1a695866960c4d9b8a
Geremy Giles <ggiles2@uccs.edu>
2026-02-27 10:33:24 -0700

    Adding Game Loop

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -8,6 +8,56 @@
 #include <iostream>
 using namespace std;
 
+// Class declarations
+class Game
+{
+private:
+
+    bool SinglePlayerGame()
+    {
+        bool gameOver = false;
+
+        while (!gameOver)
+        {
+            // Game loop
+            cout << "Single player game" << endl;
+            gameOver = true;
+        }
+
+        return true;
+    }
+
+    bool TwoPlayerGame()
+    {
+        bool gameOver = false;
+
+        while (!gameOver)
+        {
+            // Game loop
+            cout << "Two player game" << endl;
+            gameOver = true;
+        }
+
+        return false;
+    }
+
+public:
+
+    bool StartGame(int playerCount)
+    {
+        if (playerCount == 1)
+        {
+            return SinglePlayerGame();
+        }
+        else
+        {
+            return TwoPlayerGame();
+        }
+    }
+};
+
+
+
 // Prompts the user for how many players, then calls the corrosponding method in the Game class. This class contains a flag to mark which player has won.
 int main()
 {
@@ -15,38 +65,40 @@ int main()
 
     cout << "Welcome to Tic-Tac-Toe!" << endl;
 
+    // Ensure the user inputs a valid number, then break the loop
     int playerCount = 0;
-
     while (playerCount == 0) 
     {
         cout << "1 or 2 players?" << endl;
         
         int inputtedPlayers;
         cin >> inputtedPlayers;
-        if (inputtedPlayers == 1 || inputtedPlayers == 2) 
+        if (inputtedPlayers == 1 || inputtedPlayers == 2) // Valid Input
         {
             playerCount = inputtedPlayers;
         }
+        else
+        {
+            cout << "Invalid Input, please try again." << endl;
+            inputtedPlayers = 0;
+        }
     }
 
-    if (playerCount == 1) 
+    // Create game instance
+    Game game;
+
+    player1Win = game.StartGame(playerCount);
+
+    cout << "The game is over! ";
+    if (player1Win)
     {
-
+        cout << "Player 1 has won!" << endl;
     }
-
-    cout << "Hello World!\n";
+    else
+    {
+        cout << "Player 2 has won!" << endl;
+    }
+    
 }
 
 
-class Game
-{
-    private bool SinglePlayerGame()
-    {
-
-    }
-
-    private bool TwoPlayerGame()
-    {
-
-    }
-};
