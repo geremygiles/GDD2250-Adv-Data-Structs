dbc916b646832bea52ac26fa12d949999755a182
Geremy Giles <ggiles2@uccs.edu>
2026-04-10 15:08:30 -0600

    Update User_Input_Manipulation.cpp

--- Chapter Exercises/Chapter 3/User_Input_Manipulation/User_Input_Manipulation.cpp
+++ Chapter Exercises/Chapter 3/User_Input_Manipulation/User_Input_Manipulation.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream> // Input/Output
 #include <string> // Strings
+#include <cctype> // Convert to uppercase chars
 
 using namespace std;
 
@@ -21,11 +22,47 @@ int main()
     cout << "Input something fun!" << endl << endl;
 
     // Add spaces in-between characters
-    cin.get(inputChar);
-    input += " " + inputChar;
+    while (cin.peek() != '\n')
+    {
+        cin.get(inputChar);
+        input = input + inputChar + ' ';
+    }
 
     // Print to console
-    cout << input;
+    cout << input << endl << endl;
+
+    // Clear \n from input stream
+    cin.get();
+    // Clear input
+    input = "";
 
     // Convert every other character to caps (if applicable)
+
+    cout << "Input something interesting!" << endl << endl;
+    
+
+    bool caps = false;
+
+    // Add spaces in-between characters
+    
+    while (cin.peek() != '\n')
+    {
+        cin.get(inputChar);
+
+        cout << inputChar << endl;
+
+        if (inputChar > 96 && inputChar < 123) // If lowercase
+        {
+            if (caps) // Alternating Caps flag
+            {
+                inputChar -= 32;
+            }
+            caps = !caps;
+        }
+        
+        input = input + inputChar;
+    }
+
+    // Print to console
+    cout << input << endl << endl;
 }
